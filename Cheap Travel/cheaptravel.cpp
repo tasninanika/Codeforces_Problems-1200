5348871fbec81cf47766747f1e51b6ec62ff9d70
anikatasnin <jarintasnin09@gmail.com>
2025-03-13 09:51:43 +0600

    added Cheap Travel

--- Cheap Travel/cheaptravel.cpp
+++ Cheap Travel/cheaptravel.cpp
@@ -6,7 +6,7 @@ int main(){
     cin >> n >> m >> a >> b;
 
     int cost1 = n * a;
-    int cost2 = ((n + m - 1) + b);
+    int cost2 = ((n + m - 1) / m) * b;
     int cost3 = (n / m) * b + (n % m) * a;
 
     int min_cost = min(cost1, cost2, cost3);
