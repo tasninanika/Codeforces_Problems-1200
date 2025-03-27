a3b17b0847746ff19a98cb0fdcbc9c8d85882ffd
anikatasnin <jarintasnin09@gmail.com>
2025-03-27 10:32:19 +0600

    added K-th Not Divisible by n

--- K-th Not Divisible by n/K-thNotDivisibleByN.cpp
+++ K-th Not Divisible by n/K-thNotDivisibleByN.cpp
@@ -12,7 +12,7 @@ int main(){
         vector<int> arr;
         int num = 1;
         while(arr.size() < k){
-            if(num % 3 != 0){
+            if(num % n != 0){
                 arr.push_back(num);
             }
             num++;
