bfea204feba547430f155278e02946849f712676
anikatasnin <jarintasnin09@gmail.com>
2025-04-26 08:28:14 +0600

    added Worms

--- Worms/worms.cpp
+++ Worms/worms.cpp
@@ -26,6 +26,9 @@ int main(){
         int left = 0, right = n - 1; ans = -1;
         while(left <= right){
             int mid = (left + right) / 2;
+            if(w <= prefix[mid]){
+
+            }
         }
     }
 
