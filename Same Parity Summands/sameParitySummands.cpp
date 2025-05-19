86bacd98912dcd8d7b46bc448e58da235490f5f0
anikatasnin <jarintasnin09@gmail.com>
2025-05-19 13:22:01 +0600

    added Same Parity Summands

--- Same Parity Summands/sameParitySummands.cpp
+++ Same Parity Summands/sameParitySummands.cpp
@@ -1,37 +1,38 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main(){
     int t;
-    cin >> t;
+	cin >> t;
 
-    while(t--){
-        long long n, k;
-        cin >> n >> k;
+	while (t--){
+		int n, k;
+		cin >> n >> k;
 
-        if(k > n){
-            cout << "NO" << endl;
-            continue;
-        }
+		int n1 = n - (k - 1);
 
-        if((n - k + 1) % 2 == 1 && n - k + 1 > 0){
-            cout << "YES" << endl;
-            for(int i = 0; i < k - 1; i++){
-                cout << 1 << " ";
+		if (n1 > 0 && n1 % 2 == 1){
+			cout << "YES" << endl;
+
+			for (int i = 0; i < k - 1; i++){
+              cout << "1 ";
+			}
+			cout << n1 << endl;
+			continue;
+		}
+
+		int n2 = n - 2 * (k - 1);
+
+		if (n2 > 0 && n2 % 2 == 0){
+			cout << "YES" << endl;
+
+			for(int i = 0; i < k - 1; ++i){
+                cout << "2 ";
             }
-            cout << n - k + 1 << endl;
-         }
-         else if((n - 1 * (k - 1)) % 2 == 0 && (n - 2 * (k - 1)) > 0){
-            cout << "YES" << endl;
-            for(int i = 0; i < k - 1; i++){
-                cout << 2 << " ";
-            }
-            cout << n - 2 * (k - 1) << endl;
-         }
-         else{
-            cout << "NO" << endl;
-         }
-    }
-
+			cout << n2 << endl;
+			continue;
+		}
+		cout << "NO" << endl;
+	}
     return 0;
 }
