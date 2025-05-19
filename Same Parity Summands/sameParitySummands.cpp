92cd37688ca6e31e53a43c402dea5dfa22edb96e
anikatasnin <jarintasnin09@gmail.com>
2025-05-19 12:59:20 +0600

    added Same Parity Summands

--- Same Parity Summands/sameParitySummands.cpp
+++ Same Parity Summands/sameParitySummands.cpp
@@ -22,7 +22,7 @@ int main(){
             cout << n - k + 1 << endl;
          }
          else if((n - 1 * (k - 1)) % 2 == 0 && (n - 2 * (k - 1)) > 0){
-
+            cout << "YES" << endl;
          }
     }
 
