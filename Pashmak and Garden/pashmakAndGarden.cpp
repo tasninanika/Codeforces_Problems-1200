10f795738fef99c593ee5c86a82d7ffd92605f80
anikatasnin <jarintasnin09@gmail.com>
2025-05-27 20:20:28 +0600

    added Pashmak and Garden

--- Pashmak and Garden/pashmakAndGarden.cpp
+++ Pashmak and Garden/pashmakAndGarden.cpp
@@ -19,7 +19,8 @@ int main(){
             y3 = y4 = y1 + abs(x1 - x2);
             x3 = x1;
             x4 = x2;
-        }
+            cout << x3 << " " << y3 << " " << x4 << " " << y4 << endl;
+         }
 
     }
 
