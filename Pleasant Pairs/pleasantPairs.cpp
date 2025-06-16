06f1e6335e33e40f90019c8cede0ce81fefee26d
anikatasnin <jarintasnin09@gmail.com>
2025-06-16 19:49:53 +0600

    added Pleasant Pairs

--- Pleasant Pairs/pleasantPairs.cpp
+++ Pleasant Pairs/pleasantPairs.cpp
@@ -9,7 +9,30 @@ int main(){
         int n;
         cin >> n;
 
-        int arr[n];
+        int size = 2 * n + 1;
+        vector<int> ar(size, 1e18);
+
+        for (int i = 1; i <= n; i++) {
+            int x;
+            cin >> x;
+            ar[x] = i;
+        }
+
+        int c = 0;
+
+        for (int i = 3; i < size; i++) {
+            for (int j = 1; j * j < i; j++) {
+                if (i % j == 0) {
+                    int k = i / j;
+
+                    if (ar[j] + ar[k] == i) {
+                        c++;
+                    }
+                }
+            }
+        }
+
+        cout << c << endl;
     }
 
     return 0;
