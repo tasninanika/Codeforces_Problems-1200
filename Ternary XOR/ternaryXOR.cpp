ed6ac1545007b9aa27445c69e8b5f5acf693149a
anikatasnin <jarintasnin09@gmail.com>
2025-06-18 20:22:23 +0600

    added Ternary XOR

--- Ternary XOR/ternaryXOR.cpp
+++ Ternary XOR/ternaryXOR.cpp
@@ -1,14 +1,50 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main() {
     int t;
     cin >> t;
 
-    while(t--){
+    while (t--) {
         int n;
+        cin >> n;
         string str;
-        cin >> n >> str;
+        cin >> str;
+
+        string a = "";
+        string b = "";
+        int split = 0;
+
+        for (int i = 0; i < n; i++) {
+            char ch = str[i];
+
+            if (ch == '0') {
+                a += '0';
+                b += '0';
+            }
+            else if (ch == '2') {
+                if (split == 1) {
+                    a += '0';
+                    b += '2';
+                } else {
+                    a += '1';
+                    b += '1';
+                }
+            }
+            else {
+                if (split == 1) {
+                    a += '0';
+                    b += '1';
+                } else {
+                    a += '1';
+                    b += '0';
+                    split = 1;
+                }
+            }
+        }
+
+        cout << a << endl;
+        cout << b << endl;
     }
 
     return 0;
