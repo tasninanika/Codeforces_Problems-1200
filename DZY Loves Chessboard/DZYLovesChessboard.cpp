716f596e0a43730c1bd36c5e15d4b362adc6df60
anikatasnin <jarintasnin09@gmail.com>
2025-06-19 18:23:47 +0600

    added DZY Loves Chessboard

--- DZY Loves Chessboard/DZYLovesChessboard.cpp
+++ DZY Loves Chessboard/DZYLovesChessboard.cpp
@@ -11,7 +11,7 @@ int main(){
             cin >> cell;
             if(cell == '.'){
                 if((i + j) % 2 == 0){
-
+                    cout << 'B';
                 }
             }
         }
