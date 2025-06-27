0fafe3184c37684324007fe4103935f2e26cb666
anikatasnin <jarintasnin09@gmail.com>
2025-06-27 15:17:02 +0600

    added Vlad and a Sum of Sum of Digits

--- Vlad and a Sum of Sum of Digits/vladAndASumOfSumOfDigits.cpp
+++ Vlad and a Sum of Sum of Digits/vladAndASumOfSumOfDigits.cpp
@@ -1,1 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    int preSum[200001];
+
+    preSum[0] = 0;
+
+    for (int i = 1; i <= 200000; i++) {
+        int sum = 0, num = i;
+
+        while(num > 0){
+            sum += num % 10;
+            num /= 10;
+        }
+        preSum[i] = preSum[i - 1] + sum;
+    }
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        cout << preSum[n] << endl;
+    }
+
+    return 0;
+}
 
