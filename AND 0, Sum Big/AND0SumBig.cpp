1f0c6e357fdec866fdb1dbddd6059350a9716dea
anikatasnin <jarintasnin09@gmail.com>
2025-07-04 10:09:28 +0600

    added AND 0, Sum Big

--- AND 0, Sum Big/AND0SumBig.cpp
+++ AND 0, Sum Big/AND0SumBig.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    long long mod = 1000000007;
+
+    while(t--){
+        int n, k;
+        cin >> n >> k;
+
+        long long result = 1;
+
+        for(int i = 0; i < k; i++){
+            result = (result * n) % mod;
+        }
+
+        cout << result << endl;
+    }
+
+    return 0;
+}
 
