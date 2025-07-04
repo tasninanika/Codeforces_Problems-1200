1c655ce9a51026dce17e1dc86fd0e6b648fee797
anikatasnin <jarintasnin09@gmail.com>
2025-07-04 10:28:22 +0600

    added XXXXX

--- XXXXX/XXXXX.cpp
+++ XXXXX/XXXXX.cpp
@@ -1,1 +1,48 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n, x;
+        cin >> n >> x;
+
+        int a[100001];
+        long long total = 0;
+
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
+
+            total += a[i];
+        }
+
+        if(total % x != 0){
+            cout << n << endl;
+        }
+        else{
+            int left = 0, right = n - 1;
+
+            while(left < n && a[left] % x == 0){
+                left++;
+            }
+
+            while(right >= 0 && a[right] % x == 0){
+                right--;
+            }
+
+            if(left == n){
+                cout << -1 << endl;
+            }
+            else{
+                int max_len = max(n - left - 1, right);
+
+                cout << max_len << endl;
+            }
+        }
+    }
+
+    return 0;
+}
 
