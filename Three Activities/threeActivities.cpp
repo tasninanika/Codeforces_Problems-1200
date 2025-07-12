eda26e5e90760f0c8e4ed2d3a0000caf1b0c2e7f
anikatasnin <jarintasnin09@gmail.com>
2025-07-12 10:22:48 +0600

    added Three Activities

--- Three Activities/threeActivities.cpp
+++ Three Activities/threeActivities.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define ll long long
 
 int main(){
     int t;
@@ -8,6 +9,8 @@ int main(){
     while(t--){
         int n;
         cin >> n;
+
+        vector<pair<<ll, int>> row1(n), row2(n), row3(n);
     }
 
 
