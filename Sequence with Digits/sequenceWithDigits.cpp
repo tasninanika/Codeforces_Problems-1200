ce8641c5dc9d57f1593699e57ac179f4b1c79271
anikatasnin <jarintasnin09@gmail.com>
2025-07-12 11:16:05 +0600

    added Sequence with Digits

--- Sequence with Digits/sequenceWithDigits.cpp
+++ Sequence with Digits/sequenceWithDigits.cpp
@@ -1,1 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        long long a, k;
+        cin >> a >> k;
+
+        for(int i = 1; i < k; i++){
+            string s = to_string(a);
+            int min_digit = *min_element(s.begin(), s.end()) - '0';
+            int max_digit = *max_element(s.begin(), s.end()) - '0';
+
+            if(min_digit == 0){
+                break;
+            }
+            a += min_digit * max_digit;
+        }
+
+        cout << a << endl;
+    }
+
+    return 0;
+}
 
