52ba5e8b9180dcf4c398d8c49369028745158418
anikatasnin <jarintasnin09@gmail.com>
2025-07-17 18:34:30 +0600

    added k-LCM (easy version)

--- k-LCM (easy version)/k-LCM(easyversion).cpp
+++ k-LCM (easy version)/k-LCM(easyversion).cpp
@@ -6,18 +6,10 @@ int main(){
     cin >> t;
 
     while(t--){
-        int n;
-        cin >> n;
+        int n, k;
+        cin >> n >> k;
 
-        if(n % 4 == 0){
-            cout << n / 4 << " " << n / 4 << " " << n / 2 << endl;
-        }
-        else if(n % 2 != 0){
-            cout << 1 << " " << n / 2 << " " << n / 2 << endl;
-        }
-        else{
-            cout << n / 2 - 1 << " " << n / 2 - 1 << " " << 2 << endl;
-        }
+
     }
     return 0;
 }
