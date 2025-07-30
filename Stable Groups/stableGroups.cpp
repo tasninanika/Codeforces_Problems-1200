6e4158de2757894fff36cba6d9c88bf647298113
anikatasnin <jarintasnin09@gmail.com>
2025-07-30 15:14:16 +0600

    added Stable Groups

--- Stable Groups/stableGroups.cpp
+++ Stable Groups/stableGroups.cpp
@@ -1,1 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long n, k, x;
+    cin >> n >> k >> x;
+
+    vector<long long> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+
+    sort(v.begin(), v.end());
+
+    vector<long long> differences;
+
+    for (int i = 0; i < n - 1; i++){
+        if (v[i + 1] - v[i] > x){
+            differences.push_back(v[i + 1] - v[i]);
+        }
+    }
+    sort(differences.begin(), differences.end());
+
+    int ans = differences.size() + 1;
+
+    for (auto difference : differences){
+        long long val = (difference / x) + (difference % x != 0) - 1;
+
+        if (k >= val){
+            ans--;
+            k -= val;
+        }
+    }
+
+    cout << ans << endl;
+}
+
 
