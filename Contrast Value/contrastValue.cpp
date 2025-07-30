a257372f4e8ea117033d9cd03241f62656c32d14
anikatasnin <jarintasnin09@gmail.com>
2025-07-30 15:00:17 +0600

    added Contrast Value

--- Contrast Value/contrastValue.cpp
+++ Contrast Value/contrastValue.cpp
@@ -1,1 +1,45 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        int a[n];
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
+        }
+
+        if(n == 1){
+            cout << 1 << endl;
+            continue;
+        }
+
+        int ans = 1;
+        int direction = -1;
+
+        for(int i = 1; i < n; i++){
+            int current = direction;
+
+            if (a[i] > a[i - 1]) {
+                current = 0;
+            }
+            else if (a[i] < a[i - 1]) {
+                current = 1;
+            }
+            if (current != direction) {
+                ans++;
+                direction = current;
+            }
+        }
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
 
