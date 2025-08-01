1d02eaff7b4e0e074ec9dee2d6b265364a6effd6
anikatasnin <jarintasnin09@gmail.com>
2025-08-01 14:41:37 +0600

    added Right Left Wrong

--- Right Left Wrong/rightLeftWrong.cpp
+++ Right Left Wrong/rightLeftWrong.cpp
@@ -1,45 +1,41 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     int t;
     cin >> t;
 
-    while(t--){
+    while (t--) {
         int n;
         cin >> n;
 
-        vector<int> arr(n), pref(n);
-        for (int i = 0; i < n; i++) {
-            cin >> arr[i];
-        }
+        vector<long long> a(n);
+        for (int i = 0; i < n; ++i) cin >> a[i];
 
         string s;
         cin >> s;
 
-        pref[0] = arr[0];
+        vector<long long> prefix(n);
+        prefix[0] = a[0];
         for (int i = 1; i < n; i++) {
-            pref[i] = pref[i - 1] + arr[i];
+            prefix[i] = prefix[i - 1] + a[i];
         }
 
-        int c = n - 1;
+        int r = n - 1;
         long long ans = 0;
 
         for (int i = 0; i < n; i++) {
             if (s[i] == 'L') {
-                while (c > i && s[c] != 'R') {
-                    c--;
-                }
-
-                if (c > i) {
-                    int left = (i > 0) ? pref[i - 1] : 0;
-                    ans += pref[c] - left;
-                    c--;
+                while (r > i && s[r] != 'R') r--;
+                if (r > i) {
+                    long long left = (i > 0 ? prefix[i - 1] : 0);
+                    ans += prefix[r] - left;
+                    r--;
                 }
             }
         }
 
-        cout << ans << endl;
+        cout << ans << '\n';
     }
 
     return 0;
