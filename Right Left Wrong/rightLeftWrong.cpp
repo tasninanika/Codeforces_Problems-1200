98251ee5a7549a6e355e1f6b8a7e01c7eab36d4c
anikatasnin <jarintasnin09@gmail.com>
2025-08-01 14:33:43 +0600

    added Right Left Wrong

--- Right Left Wrong/rightLeftWrong.cpp
+++ Right Left Wrong/rightLeftWrong.cpp
@@ -2,51 +2,45 @@
 using namespace std;
 
 int main(){
-  int t;
-  cin >> t;
+    int t;
+    cin >> t;
 
-  while(t--){
-    int n;
-    cin >> n;
+    while(t--){
+        int n;
+        cin >> n;
 
-    vector<int> a(n);
-    for(int i = 0; i < n; ++i){
-      cin >> a[i];
-    }
+        vector<int> arr(n), pref(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
 
-    string s;
-    cin >> s;
+        string s;
+        cin >> s;
 
-    long long ans = 0;
-    int l = 0, r = n - 1;
-    while(l < r){
-      while(l < n && s[l] == 'R'){
-        l++;
-      }
-      while(r >= 0 && s[r] == 'L'){
-        r--;
-      }
+        pref[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            pref[i] = pref[i - 1] + arr[i];
+        }
 
-      if(l < r){
-        for(int i = l; i <= r; ++i){
-            if(s[i] == 'R'){
-                for(int j = i; j <= r; ++j){
-                   if(s[j] == 'L'){
-                        ans += a[j] - a[l];
-                        l = j + 1;
-                        r = j-1;
-                        break;
-                    }
+        int c = n - 1;
+        long long ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] == 'L') {
+                while (c > i && s[c] != 'R') {
+                    c--;
                 }
-                break;
+
+                if (c > i) {
+                    int left = (i > 0) ? pref[i - 1] : 0;
+                    ans += pref[c] - left;
+                    c--;
+                }
             }
         }
 
-      }
-
-
+        cout << ans << endl;
     }
-    cout << ans << endl;
-  }
-  return 0;
+
+    return 0;
 }
