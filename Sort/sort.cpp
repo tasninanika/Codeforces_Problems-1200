1c7ab07e3726ae058a67837b8ce3db46cafc2f84
anikatasnin <jarintasnin09@gmail.com>
2025-08-08 11:37:25 +0600

    added Sort

--- Sort/sort.cpp
+++ Sort/sort.cpp
@@ -1,1 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+
+    while(cin >> n){
+        vector<string> words;
+
+        for (int i = 0; i < n; ++i) {
+            string s;
+            cin >> s;
+            words.push_back(s);
+        }
+
+        sort(words.begin(), words.end(), [](const string &a, const string &b) {
+            return a.size() < b.size();
+        });
+
+        bool valid = true;
+        for (int i = 0; i < n - 1; ++i) {
+            if (words[i + 1].find(words[i]) == string::npos) {
+                valid = false;
+                break;
+            }
+        }
+
+        if (valid) {
+            cout << "YES" << endl;
+            for (int i = 0; i < n; ++i) {
+                cout << words[i] << endl;
+            }
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
 
