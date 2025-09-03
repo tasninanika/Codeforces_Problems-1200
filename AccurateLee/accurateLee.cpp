e0bed97b21a460e887f1ed85a1948873ce083687
anikatasnin <jarintasnin09@gmail.com>
2025-09-03 10:00:49 +0600

    added AccurateLee

--- AccurateLee/accurateLee.cpp
+++ AccurateLee/accurateLee.cpp
@@ -9,31 +9,33 @@ void solve(){
     cin >> s;
 
     int leading_zeros = 0;
-    while (leading_zeros < n && s[leading_zeros] == '0') {
+    while(leading_zeros < n && s[leading_zeros] == '0'){
         leading_zeros++;
     }
 
     int trailing_ones = 0;
-    while (trailing_ones < n && s[n - 1 - trailing_ones] == '1') {
+    while(trailing_ones < n && s[n - 1 - trailing_ones] == '1'){
         trailing_ones++;
     }
 
-    if (leading_zeros + trailing_ones < n) {
-        for (int i = 0; i < leading_zeros; ++i) {
+    if(leading_zeros + trailing_ones < n){
+        for(int i = 0; i < leading_zeros; ++i){
             cout << '0';
         }
         cout << '0';
-        for (int i = 0; i < trailing_ones; ++i) {
+
+        for(int i = 0; i < trailing_ones; ++i){
             cout << '1';
         }
+
         cout << endl;
     }
-    else {
+    else{
         cout << s << endl;
     }
 }
 
-int main() {
+int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int t;
